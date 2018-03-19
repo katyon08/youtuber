ee17aff00f4d927ac3e2f040cfc9e6501adf9606
vrossokhovatskiy <vrossokhovatskiy@dataart.com>
2018-03-19 16:57:18 +0200

    fix

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 import os
+
+import pytube
 import requests
 import datetime
 
@@ -53,39 +55,80 @@ class BotHandler:
 
 
 greet_bot = BotHandler(token)
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
 now = datetime.datetime.now()
 
 
 def main():
+    last = None
+    try:
+        while True:
+            greet_bot.get_updates()
+            last_update = greet_bot.get_last_update()
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-    while True:
-        greet_bot.get_updates()
-        last_update = greet_bot.get_last_update()
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+            if last is None or last_chat_text not in last:
 
-        greet_bot.send_message(last_chat_id, last_update)
+                if last is None:
+                    greet_bot.send_message(last_chat_id, u"Пришли мне ссылку на ютуб")
+                    last = last_chat_text
+                else:
+                    link = last_chat_text
+                    # "https://www.youtube.com/watch?v=rfOY8ePOs_0"
 
-        link = last_chat_text
-        # "https://www.youtube.com/watch?v=rfOY8ePOs_0"
-        greet_bot.send_message(last_chat_id, "processing: " + link)
+                    try:
+                        yt = YouTube(link)
 
-        yt = YouTube(link)
+                        if not exist(os.getcwd(), yt.title):
+                            try:
+                                yt.streams.filter(only_audio=True, subtype='mp4').order_by('resolution').first().download(os.getcwd())
+                            except KeyError:
+                                greet_bot.send_message(last_chat_id,
+                                                       u"OOPS! С этим видео что-то не так! Скинь эту ссылку на katyon08@yandex.ru, мой хазяин попытается это починить!")
 
-        greet_bot.send_message(last_chat_id, "downloading: " + yt.title)
+                            except RegexMatchError:
+                                greet_bot.send_message(last_chat_id,
+                                                       u"OOPS! С этим видео что-то не так! Скинь эту ссылку на katyon08@yandex.ru, мой хазяин попытается это починить!")
 
-        yt.streams.filter(only_audio=True, subtype='mp4').order_by('resolution').first().download(os.getcwd())
+                            except AttributeError:
+                                greet_bot.send_message(last_chat_id,
+                                                       u"OOPS! С этим видео что-то не так! Скинь эту ссылку на katyon08@yandex.ru, мой хазяин попытается это починить!")
 
-        greet_bot.send_message(last_chat_id, "downloaded: " + os.path.join(os.getcwd(), yt.title))
+                            except UnicodeDecodeError:
+                                greet_bot.send_message(last_chat_id,
+                                                       u"OOPS! С этим видео что-то не так! Скинь эту ссылку на katyon08@yandex.ru, мой хазяин попытается это починить!")
 
-        bot.send_document(last_chat_id, document=open(os.path.join(os.getcwd(), yt.title + ".mp4"), 'rb'))
+                            except TypeError:
+                                greet_bot.send_message(last_chat_id,
+                                                       u"OOPS! С этим видео что-то не так! Скинь эту ссылку на katyon08@yandex.ru, мой хазяин попытается это починить!")
 
-        greet_bot.send_message(last_chat_id, "sent: " + open(os.path.join(os.getcwd(), yt.title + ".mp4"), 'rb').name)
+                        for root, dirs, files in os.walk(os.getcwd()):
+                            for file in files:
+                                if yt.title in file:
+                                    try:
+                                        bot.send_document(last_chat_id, document=open(file, 'rb'))
+                                        bot.send_audio(last_chat_id, audio=open(file, 'rb'))
+                                        bot.send_voice(last_chat_id, voice=open(file, 'rb'))
+                                    except telegram.error.TimedOut:
+                                        pass
+                    except pytube.exceptions.RegexMatchError:
+                        greet_bot.send_message(last_chat_id,
+                                               u"Я не вижу тут видео")
+
+                    last = None
+    except StandardError:
+        pass
 
 
+
+def exist(in_path, in_file):
+    for root, dirs, files in os.walk(in_path):
+        for file in files:
+            if in_file in file:
+                return True
+    return False
 
 
 
